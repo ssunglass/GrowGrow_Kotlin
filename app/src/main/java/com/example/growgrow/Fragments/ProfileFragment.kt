3e0c8c245939491f55a9499f7a4d05d214ab882c
이경민 <leegm218@naver.com>
2021-07-31 18:53:58 +0900

    viewBinding in ProfileFragment && add edit_btn move event to EditProfileActivity.

--- app/src/main/java/com/example/growgrow/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/growgrow/Fragments/ProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.example.growgrow.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.growgrow.EditProfileActivity
 import com.example.growgrow.R
+import com.example.growgrow.databinding.FragmentProfileBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,21 +22,29 @@ private const val ARG_PARAM2 = "param2"
  */
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var fragmentProfileBinding : FragmentProfileBinding? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?): View?
+    {
+        // Inflate the layout for this fragment
+        val binding = FragmentProfileBinding.inflate(inflater, container, false)
+        fragmentProfileBinding = binding
+
+        binding.editBtn.setOnClickListener{
+               startActivity(Intent(context, EditProfileActivity::class.java))
+
         }
+
+        return  fragmentProfileBinding!!.root
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onDestroyView() {
+        fragmentProfileBinding= null
+        super.onDestroyView()
     }
 
     companion object {
@@ -45,14 +56,6 @@ class ProfileFragment : Fragment() {
          * @param param2 Parameter 2.
          * @return A new instance of fragment ProfileFragment.
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                ProfileFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+
     }
 }
