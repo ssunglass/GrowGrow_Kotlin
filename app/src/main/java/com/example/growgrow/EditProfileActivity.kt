3abb450eead291b0fb2d001cd8bdaad700bf7a43
이경민 <leegm218@naver.com>
2021-09-07 18:37:08 +0900

    Parse Depart Json

--- app/src/main/java/com/example/growgrow/EditProfileActivity.kt
+++ app/src/main/java/com/example/growgrow/EditProfileActivity.kt
@@ -1,19 +1,25 @@
 package com.example.growgrow
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import com.example.growgrow.Model.User
 import com.example.growgrow.databinding.ActivityEditProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import kotlin.concurrent.thread
+
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -21,11 +27,17 @@ class EditProfileActivity : AppCompatActivity() {
     private var _binding: ActivityEditProfileBinding? = null
     private val binding get() = _binding!!
 
+
+
     private lateinit var firebaseUser: FirebaseUser
     private lateinit var region: String
+    private lateinit var depart: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        depart = ""
 
         _binding =  ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -38,9 +50,24 @@ class EditProfileActivity : AppCompatActivity() {
 
         userInfo()
 
+
+
+            getJson()
+
+
+
+
+
+
+
+
         binding.saveEditBtn.setOnClickListener{
             updateUserInfo()
 
+        }
+
+        binding.closeEditBtn.setOnClickListener {
+            onBackPressed()
         }
 
 
@@ -60,7 +87,7 @@ class EditProfileActivity : AppCompatActivity() {
     private fun userInfo(){
 
         FirebaseFirestore.getInstance()
-                .collection("Users").document(firebaseUser.uid).get().addOnSuccessListener{  document->
+                .collection("Users").document(firebaseUser.uid).get().addOnSuccessListener{ document->
 
                     if(document != null) {
 
@@ -104,7 +131,7 @@ class EditProfileActivity : AppCompatActivity() {
 
     private fun setupSpinnerRegion(){
         val region = resources.getStringArray(R.array.region_array)
-        val adapter = ArrayAdapter(this , R.layout.support_simple_spinner_dropdown_item, region)
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, region)
 
         binding.regionSpinner.adapter = adapter
 
@@ -114,7 +141,7 @@ class EditProfileActivity : AppCompatActivity() {
 
     private fun setupSpinnerDepart(){
         val depart = resources.getStringArray(R.array.depart_array)
-        val adapter = ArrayAdapter(this , R.layout.support_simple_spinner_dropdown_item, depart)
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, depart)
 
         binding.departSpinner.adapter = adapter
 
@@ -169,9 +196,78 @@ class EditProfileActivity : AppCompatActivity() {
     }
 
 
+    private fun getJson(){
+
+        thread(start = true){
+
+            val stream = URL("https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list").openStream()
+            val read = BufferedReader(InputStreamReader(stream, "UTF-8"))
+            depart  = read.readLine()
+
+            Log.d("DEPART", depart)
 
 
+
+        }
+
+
+
+        runOnUiThread{
+
+
+        }
+
+    }
+
+
+
+    /*
+    private fun getDangerGrade(startLat: String, startLng: String, endLat: String, endLng: String) {
+        lateinit var temp : String
+        class getDangerGrade : AsyncTask<Void, Void, Void>(){
+            override fun doInBackground(vararg params: Void?): Void? {
+                val stream = URL("https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=8fa1b6fffaf969b85712d6ea45a921fd&svcType=api&svcCode=MAJOR&contentType=json&gubun=univ_list").openStream()
+                val read = BufferedReader(InputStreamReader(stream, "UTF-8"))
+                temp  = read.readLine()
+
+                return null
+            }
+
+            override fun onPostExecute(result: Void?) {
+                val grade : String
+                super.onPostExecute(result)
+                val json = JSONObject(temp)
+                if(json.get("resultCode") != "10") {
+                    val chiefObject = (json["items"] as JSONObject)
+                    val upperArray : JSONArray = chiefObject.getJSONArray("item")
+                    val upperObject = upperArray.getJSONObject(0)
+                    grade = upperObject.getString("anals_grd")
+
+                }
+                else
+                    grade = "none"
+            }
+
+
+
+
+        }
+        getDangerGrade().execute()
+    }
+
+
+     */
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
